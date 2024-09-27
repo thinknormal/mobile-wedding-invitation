0073ff4c9b4e488d9b64b483c0305ba1f101413d
everpresent@hanmail.net <everpresent@hanmail.net>
2024-09-27 20:24:22 +0900

    update info for my mobile invitation

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,21 @@
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
+    apiKey: "AIzaSyAdTVT0Gcreagomw8kkZoJ0Sl4LhxOrVhQ",
+    authDomain: "mobile-wedding-invitatio-535c2.firebaseapp.com",
+    databaseURL: "https://mobile-wedding-invitatio-535c2-default-rtdb.firebaseio.com",
+    projectId: "mobile-wedding-invitatio-535c2",
+    storageBucket: "mobile-wedding-invitatio-535c2.appspot.com",
+    messagingSenderId: "504297080797",
+    appId: "1:504297080797:web:5276862d652a2eee8b9834",
+    measurementId: "G-941GBPMC9Z"
+  };
 
-};
-
+// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(firebaseApp);
 export const realtimeDb = getDatabase(firebaseApp);
--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,79 +1,67 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
+import image01 from '@/assets/images/001.png'
+import image02 from '@/assets/images/002.png'
+import image03 from '@/assets/images/003.png'
+import image04 from '@/assets/images/004.png'
+import image05 from '@/assets/images/005.png'
+import image06 from '@/assets/images/006.png'
+import image07 from '@/assets/images/007.png'
+import image08 from '@/assets/images/008.png'
+import image09 from '@/assets/images/009.png'
+import image10 from '@/assets/images/010.png'
 
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
     alt: 'image01',
     source: image01,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },
   {
     alt: 'image02',
     source: image02,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },
   {
     alt: 'image03',
     source: image03,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image04',
     source: image04,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image05',
     source: image05,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image06',
     source: image06,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image07',
     source: image07,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image08',
     source: image08,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image09',
     source: image09,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   },{
     alt: 'image10',
     source: image10,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image11',
-    source: image11,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image12',
-    source: image12,
-    width: 640,
-    height: 960
+    width: 1080,
+    height: 1080
   }
 ];
 
